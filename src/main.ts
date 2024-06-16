ad9373a32aebd6c2bcc786ed544bfe8f8b6ac629
Rafael Angonese <rafael.angonese09@gmail.com>
2024-06-16 01:16:34 -0300

    :sparkles: Feat(main): use parser into main fn

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,17 @@
 import { getInput } from "./input/get-input";
 import { Lexer } from "./lexer/lexer";
+import { Parser } from "./parser/parser";
 
 const input = getInput({ fileName: 'input.vivi' });
+// const input = '$<vivi> idade : int = 3; function lala ( name: int ) : int { return name ; } $</vivi>';
 
 const lexer = new Lexer(input);
 
-console.log(lexer)
-console.log(lexer.nextToken())
-console.log(lexer)
-console.log(lexer.nextToken())
-console.log(lexer.nextToken())
-console.log(lexer.nextToken())
-console.log(lexer.nextToken())
-console.log(lexer.nextToken())
-console.log(lexer.nextToken())
-console.log(lexer.nextToken())
-console.log(lexer.nextToken())
-console.log(lexer.nextToken())
-console.log(lexer.nextToken())
-console.log(lexer.nextToken())
-console.log(lexer.nextToken())
-console.log(lexer.nextToken())
-console.log(lexer.nextToken())
-console.log(lexer.nextToken())
-console.log(lexer.nextToken())
-console.log(lexer.nextToken())
-console.log(lexer.nextToken())
+// const tokens = lexer.tokenize();
+// console.log(tokens)
+
+const parser = new Parser(lexer);
+
+const tree = parser.parse();
+
+console.log(tree)
