a07abcd201d86877b2a9f5df14ad20e2757b117f
Rafael Angonese <rafael.angonese09@gmail.com>
2024-06-24 23:29:41 -0300

    :sparkles: Feat(documentation): customize 404 page

--- packages/documentation/app/not-found.tsx
+++ packages/documentation/app/not-found.tsx
@@ -1,16 +1,18 @@
 import { buttonVariants } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="min-h-[99vh] px-2 py-8 flex flex-col gap-3 items-start">
+    <div className="min-h-[99vh] px-2 py-8 flex flex-col gap-3 justify-center items-center text-center">
       <div>
         <h2 className="text-5xl font-bold">404</h2>
-        <p className="text-muted-foreground">Page not found</p>
+        <p className="text-muted-foreground">Página não encontrada</p>
       </div>
 
       <Link href="/" className={buttonVariants({})}>
-        Back to homepage
+      <ArrowLeft />{" "}
+        voltar para página inicial
       </Link>
     </div>
   );
